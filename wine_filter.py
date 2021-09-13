8368ad5120a99eb96b529eab3d5908de96a2f5a4
vodiniz <vitor.oliveira.vod@gmail.com>
2021-09-13 20:34:23 -0300

    fix price

--- wine_filter.py
+++ wine_filter.py
@@ -1,14 +1,15 @@
 #get all wine type 
-import requests
+import requests, json
 from bs4 import BeautifulSoup
 
 
 class Wine():
-    def __init__(self, name, price, score, url, wine_type, bundle):
+    def __init__(self, name, price, score, url, country, wine_type, bundle):
         self.name = name
         self.price = price
         self.score = score
         self.url = url
+        self.country = country
         self.type = wine_type
         self.bundle = bundle
         self.bundle_wine_list = None
@@ -21,28 +22,12 @@ class Wine():
             for wine in wine_list:
                 wine.append(self.bundle_wine_list)
 
-            
-
-
-url = "https://www.wine.com.br/vinhos/cVINHOS-p1.html"
-url_request = requests.get(url)
-soup = BeautifulSoup(url_request.content,'html.parser')
-
-out_of_url = "https://www.wine.com.br/vinhos/cVINHOS-p104.html"
-out_of_url_request = requests.get(out_of_url)
-soup2 = BeautifulSoup(out_of_url_request.content,'html.parser')
-import requests
 
 def check_invalid_page(page_soup, out_of_page):
     url_error = page_soup.find('h2',{'class':'Title-title'}).getText().rstrip()
     if url_error == 'erro 400':
         return True
 
-def get_wine_details(url):
-    url_request = requests.get(url)
-    soup = BeautifulSoup(url_request.content, "html.parser")
-    name = soup.find('h1',{'class':'PageHeader-title w-title--4  text-center '})
-    print(name)
 
 def get_wine_url_list(page_number):
     url = "https://www.wine.com.br/vinhos/cVINHOS-p{}.html".format(page_number)
@@ -54,6 +39,18 @@ def get_wine_url_list(page_number):
         wine_url_list.append('https://www.wine.com.br'+wine.find('a',{'class':'js-productClick'}).get('href'))
     print(wine_url_list)
     
+def get_wine_details(url):
+    url_request = requests.get(url)
+    soup = BeautifulSoup(url_request.content, "html.parser")
+    name = (soup.find('div',{'class':'hidden-xs hidden-sm'})).\
+        find('h1').get_text()
+    #print(name)
+    price = (soup.find('price-box')).attrs
+    for key in price.items():
+        print(key)
+
+
+
 
 
 
